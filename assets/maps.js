dcb726e0a8fc521afdd6389191186ca5e41381fd
Ubuntu <ubuntu@ip-172-31-31-161.ec2.internal>
2019-07-13 14:17:27 +0000

    maps.js - added function to loading the location information's

--- assets/maps.js
+++ assets/maps.js
@@ -212,4 +212,23 @@ function reset() {
         buildIWContent(place);
       });
   }
+  
+    // Loading the location information's into the HTML elements used by the info window
+  function buildIWContent(place) {
+    if (place) {
+      document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' + 'src="' + place.icon + '"/>';
+      document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url + '">' + place.name + '</a></b>';
+      document.getElementById('iw-address').textContent = place.vicinity;
+    }
+
+    if (place.formatted_phone_number) {
+      document.getElementById('iw-phone-row').style.display = '';
+      document.getElementById('iw-phone').textContent =
+        place.formatted_phone_number;
+    }
+
+    else {
+      document.getElementById('iw-phone-row').style.display = 'none';
+    }
+
 		  
