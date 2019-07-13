8e0a00967b33947d7e0572f46483776e4aa84e8a
Ubuntu <ubuntu@ip-172-31-31-161.ec2.internal>
2019-07-13 14:21:23 +0000

    maps.js - added function for ratting places

--- assets/maps.js
+++ assets/maps.js
@@ -231,4 +231,38 @@ function reset() {
       document.getElementById('iw-phone-row').style.display = 'none';
     }
 
+    // Ratting places 
+
+    if (place.rating) {
+      var ratingHtml = '';
+      for (var i = 0; i < 5; i++) {
+        if (place.rating < (i + 0.5)) {
+          ratingHtml += '&#10025;';
+        }
+        else {
+          ratingHtml += '&#10029;';
+        }
+        document.getElementById('iw-rating-row').style.display = '';
+        document.getElementById('iw-rating').innerHTML = ratingHtml;
+      }
+    }
+
+    else {
+      document.getElementById('iw-rating-row').style.display = 'none';
+    }
+
+    if (place.website) {
+      var fullUrl = place.website;
+      var website = hostnameRegexp.exec(place.website);
+      if (website === null) {
+        website = 'http://' + place.website + '/';
+        fullUrl = website;
+      }
+      document.getElementById('iw-website-row').style.display = '';
+      document.getElementById('iw-website').textContent = website;
+    }
+    else {
+      document.getElementById('iw-website-row').style.display = 'none';
+    }
+  }
 		  
